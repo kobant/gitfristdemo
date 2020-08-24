f443c67dc7a1e040c22f16738b00daa4feced25f
kobant <2455876474@qq.com>
2020-08-24 14:36:20 +0800

    添加文件

---
+++ GitFristDemo01/src/main/java/helloGit.java
@@ -0,0 +1,13 @@
+/**
+ * /**
+ *
+ * @author 廖聪聪
+ * @version 1.0
+ * 创建时间 2020/8/24 14:25
+ */
+
+public class helloGit {
+	public static void main(String[] args) {
+		System.out.println("Heloo Git");
+	}
+}
